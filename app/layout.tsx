269ed99e7d07d178dcd4e67833167896f78b33a6
Abdullah <abdullahmug2332@gmail.com>
2026-01-20 22:34:54 +0500

    make redux presisted

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,19 @@
 import "./globals.css";
 import { Barlow, Inter } from 'next/font/google'
 import { Provider } from "react-redux";
-import { store } from "../store/store";
-
+import { store, persistor } from "../store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const barlow = Barlow({
   subsets: ['latin'],
   weight: [
-    '100','200','300','400','500','600','700','800','900'
+    '100', '200', '300', '400', '500', '600', '700', '800', '900'
   ],
   style: ['normal', 'italic'],
 })
 const inter = Inter({
   subsets: ['latin'],
-  weight: ['300','400','500','600','700'],
+  weight: ['300', '400', '500', '600', '700'],
   variable: '--font-inter',
 })
 
@@ -30,13 +30,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <title>Acadex | Track your academic progress</title>
-      <meta name="description" content="A learning managment system" />
+        <title>Acadex | Track your academic progress</title>
+        <meta name="description" content="A learning managment system" />
       </head>
       <body
         className={` ${barlow.className} ${inter.variable} antialiased`}
       >
-        <Provider store={store}>{children}</Provider>;
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            {children}
+          </PersistGate>
+        </Provider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,31 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useAppSelector } from "@/store/hooks";
-
-
-
+import { useAppDispatch } from "@/store/hooks";
+import { clearUser } from "@/store/userSlice";
 
 export default function Home() {
   const user = useAppSelector((state) => state.user.user);
   const router = useRouter();
+  const dispatch = useAppDispatch();
+
   const handleLogout = async () => {
-    await fetch("/api/auth/logout", {
+    await fetch("/api/logout", {
       method: "POST",
     });
+
+    dispatch(clearUser());
     router.push("/auth");
     router.refresh();
   };
 
 
 
-  if (!user) return <p>User not logged in</p>; // Only shows if fetch fails
+  if (!user) return <p>User not logged in</p>;
   return (
     <div>
       <p>Welcome {user.name}</p>
-      <p>Id: {user.id}</p>  
+      <p>Id: {user.id}</p>
       <button
         onClick={handleLogout}
         className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
